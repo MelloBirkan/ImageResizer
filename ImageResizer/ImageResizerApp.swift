44ccd5d984155d6dc9843f77b8d1f045de8b4906
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-12-30 00:51:21 -0300

    Initial Commit

---
+++ ImageResizer/ImageResizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageResizerApp.swift
+//  ImageResizer
+//
+//  Created by Marcello on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageResizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
